f68cfb294c65b53e1b5d3ade74db03b244151bc8
Shush <almogosheriko@gmail.com>
2021-12-01 10:03:05 +0200

    Day 1

--- solutions/day1.ts
+++ solutions/day1.ts
@@ -4,7 +4,49 @@ export function solution(input: string): {
 	result1: Solution;
 	result2: Solution;
 } {
-	// do stuff
+	const parsedInput = input.split("\n").map((num) => parseInt(num, 10));
 
-	return { result1: 0, result2: 0 };
+	// Part 1
+	let currDepth;
+	let result1Count = 0;
+	for (const depth of parsedInput) {
+		if (currDepth === undefined) {
+			currDepth = depth;
+			continue;
+		}
+
+		if (depth > currDepth) result1Count++;
+		currDepth = depth;
+	}
+
+	// Part 2
+	let currDepthSum;
+	let result2Count = 0;
+	for (let index = 0; index < parsedInput.length; index++) {
+		// Ignore last measurements that cannot be summed
+		if (index + 2 >= parsedInput.length) continue;
+
+		const sum =
+			parsedInput[index] +
+			parsedInput[index + 1] +
+			parsedInput[index + 2];
+		if (currDepthSum === undefined) {
+			currDepthSum = sum;
+			continue;
+		}
+
+		if (sum > currDepthSum) result2Count++;
+		currDepthSum = sum;
+	}
+
+	return { result1: result1Count, result2: result2Count };
 }
+
+// 100 [0]
+// 105 [1]
+// 180 [2]
+
+// length: 3
+
+// 2 < 3 works
+// 3 < 3 not works
